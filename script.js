5abd8527b9ece4921887090888d45431c9ee6964
vixzzhost <fikrichamdani0@gmail.com>
2025-12-12 18:09:43 +0700

    Add styles for foto and anggota components

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+}
+
+/* FOTO */
+.foto {
+    width: 100%;
+    height: 150px;
+    object-fit: cover;
+    border-radius: 6px;
+}
+
+/* ANGGOTA */
+.anggota-box {
+    display: flex;
+    gap: 10px;
+    align-items: center;
+}
+
+.anggota-box img {
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+    object-fit: cover;
+}
+
 
